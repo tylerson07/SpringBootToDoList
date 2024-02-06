03fd7cbf51bbcccb5291de0c8e70256c8be7c085
tyler <smooky0702@gmail.com>
2024-02-06 19:36:31 +0900

    commit and push

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -1,2 +1,78 @@
-package com.sparta.springauth.controller;public class UserController {
+package com.sparta.springauth.controller;
+
+import com.sparta.springauth.dto.LoginRequestDto;
+import com.sparta.springauth.dto.SignupRequestDto;
+import com.sparta.springauth.entity.User;
+import com.sparta.springauth.repository.UserRepository;
+import com.sparta.springauth.service.UserService;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/api")
+public class UserController {
+
+    public UserController(UserService userService, UserRepository userRepository) {
+        this.userService = userService;
+        this.userRepository = userRepository;
+    }
+
+    private final UserService userService;
+    private final UserRepository userRepository;
+
+    @GetMapping("/user/login-page")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/user/signup")
+    public String signupPage() {
+        return "signup";
+    }
+
+    @PostMapping("/user/signup")
+    public String signup(SignupRequestDto requestDto){
+    userService.signup(requestDto);
+            return "redirect:/api/user/login-page";
+    }
+
+
+    @PostMapping("/user/login")
+        public String login(LoginRequestDto requestDto, HttpServletResponse res){
+            try {
+                userService.login(requestDto,res);
+            } catch (Exception e) {
+               return "redirect:/api/user/login-page?error";
+            }
+
+            return "redirect:/api/products";
+        }
+User user = new User();
+    @GetMapping("/products")
+    public String getProducts(HttpServletRequest req) {
+
+        user = (User) req.getAttribute("user");
+
+
+        return "redirect:/";
+    }
+
+@PutMapping("/update")
+    public void update(@RequestBody User user) {
+     Optional<User> UserToEdit = userRepository.findByUsername(user.getUsername());
+           if(UserToEdit.isPresent()){
+               User NewUser = UserToEdit.get();
+               // 변경할 내용
+                userRepository.save(user);//저장
+
+           }
+
+    }
+
+
 }
--- src/main/java/com/sparta/springauth/repository/UserRepository.java
+++ src/main/java/com/sparta/springauth/repository/UserRepository.java
@@ -1,2 +1,15 @@
-package com.sparta.springauth.repository;public interface UserRepository {
+package com.sparta.springauth.repository;
+
+import com.sparta.springauth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+   Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+
 }
